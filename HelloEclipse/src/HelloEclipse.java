440fdd1c9c77c287b1cc07a56c2de76f7765c205
anirudhac <anirudha.choudhari@gmail.com>
2012-10-03 18:50:48 +0530

    First change done

--- HelloEclipse/src/HelloEclipse.java
+++ HelloEclipse/src/HelloEclipse.java
@@ -5,7 +5,7 @@ public class HelloEclipse {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Git is fun");
+		System.out.println("Git is fun... Eclipse is cool!");
 	}
 
 }
