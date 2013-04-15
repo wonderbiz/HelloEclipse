c2747394367440c65fa452683ffc5e10dc22b9df
Anirudha Choudhari <anirudhac@ubuntu-AVC.(none)>
2013-04-15 16:21:35 +0530

    Adding JUnit TestCase

---
+++ HelloEclipse/test/com/wonderbiz/lib/maths/MathsTest.java
@@ -0,0 +1,15 @@
+package com.wonderbiz.lib.maths;
+
+import junit.framework.TestCase;
+
+public class MathsTest extends TestCase {
+
+	public final void testAdd() {
+		assertTrue(true);
+	}
+
+	public final void testSubtract() {
+		fail("Not yet implemented");
+	}
+
+}
